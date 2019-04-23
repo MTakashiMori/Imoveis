1911ad242ee27558a3406c2a23183da748325a92
Marcelo Takashi <Spenttie@gmail.com>
2019-04-23 09:55:51 -0300

    config set title

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,13 +1,21 @@
 <template>
-    <v-card class="elevation-7">
-        <a><router-link to="/">Go to Home</router-link></a><br>
-        <a><router-link to="/about">Go to About</router-link></a>
-        <router-view></router-view>
+    <v-card class="elevation-7" id="content">
+        <div class="container">
+            <h1>{{ title }}</h1>
+            <a><router-link to="/">Go to Home</router-link></a><br>
+            <a><router-link to="/about">Go to About</router-link></a>
+            <router-view></router-view>
+        </div>
     </v-card>
 </template>
 
 <script>
 export default {
-    name: 'Content'
+    name: 'Content',
+    data() {
+        return {
+            title: 'Conteúdo'
+        }
+    }
 }
 </script>
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,13 +1,6 @@
 <template>
     <v-toolbar>
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
-    </v-toolbar-items>
+    <v-toolbar-title>Imóveis</v-toolbar-title>
   </v-toolbar>
 </template>
 
